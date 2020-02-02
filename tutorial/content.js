d2375f4be3198e967075808a67d87a1561faf221
Blair Niu <shuningn@usc.edu>
2020-02-02 00:34:33 -0800

    a beautiful front end update im cryi nn

--- tutorial/content.js
+++ tutorial/content.js
@@ -83,6 +83,55 @@ setInterval(function() {
 			numCensored++;
 		}
 	}
+	comments = document.getElementsByClassName("Mr508");
+	var numCensored = 0;
+	for(var i = 0; i < comments.length; i++) {
+		var censoredYet = 0;
+		if(rBool == 1) {
+			censoredYet = 1;
+			comments[i].innerHTML = "This comment was suspended because it was deemed racist. Found a variation of: \"" + racism[j] + "\"";
+			comments[i].style = "color: #CE0D00; font-weight: bold";
+		}
+		if(pBool == 1) {
+			for(var j = 0; j < profanity.length; j++) {
+				if(comments[i].innerHTML.toLowerCase().indexOf(profanity[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed profane. Found a variation of: \"" + profanity[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
+			}
+		}
+		if(hBool == 1) {
+			for(var j = 0; j < homophobia.length; j++) {
+				if(comments[i].innerHTML.toLowerCase().indexOf(homophobia[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed homophobic. Found a variation of: \"" + homophobia[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
+			}
+		}
+		if(sBool == 1) {
+			for(var j = 0; j < sexism.length; j++) {
+				if(comments[i].innerHTML.toLowerCase().indexOf(sexism[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed sexist. Found a variation of: \"" + sexism[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
+			}
+		}
+		if(vBool == 1) {
+			for(var j = 0; j < violence.length; j++) {
+				if(comments[i].innerHTML.toLowerCase().indexOf(violence[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed violent. Found a variation of: \"" + violence[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
+			}
+		}
+		if(censoredYet == 1) {
+			numCensored++;
+		}
+	}
 	if(numCensored != 0) {
 		document.title = "CENSORED: " + numCensored;
 	}
